4ae00c86feb33e084fb7e1899d2a40e59debcecf
ezra <pacifickwizera0@gmail.com>
2019-09-09 00:22:22 +0200

    emergency responder login and registration

---
+++ app/src/main/java/com/owulanii/androidlogin/Utils.java
@@ -0,0 +1,9 @@
+package com.owulanii.androidlogin;
+
+
+
+public class Utils {
+    public static final String BASE_IP          = "http://192.168.8.100/";
+    public static final String LOGIN_URL        = BASE_IP + "authentication/login.php";
+    public static final String REGISTER_URL     = BASE_IP + "authentication/register.php";
+}
