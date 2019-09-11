eac6a587f1cb1a26a792b7c3f945d44c4b58e74f
pro <pro@prodeMacBook-Pro.local>
2019-09-11 09:34:25 -0700

    app with db_bkd

--- emergencyresponder.sql
+++ emergencyresponder.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 09, 2019 at 12:44 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Host: localhost:8889
+-- Generation Time: Sep 11, 2019 at 04:33 PM
+-- Server version: 5.7.25
+-- PHP Version: 7.3.1
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `emergencyresponder`
@@ -26,25 +20,41 @@ SET time_zone = "+00:00";
 -- Table structure for table `responder`
 --
 
-CREATE TABLE IF NOT EXISTS `responder` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `responder` (
+  `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `token` text NOT NULL,
+  `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `responder`
 --
 
-INSERT INTO `responder` (`id`, `username`, `email`, `password`, `date_created`) VALUES
-(1, 'ezra', 'pacsonlose@gmail.com', '12345', '0000-00-00 00:00:00'),
-(2, 'hospital', 'rubavuhospital@gmail.com', '12345', '2019-09-08 16:44:03'),
-(3, 'kacyiruhospital', 'kacyiruho@gmail.com', '12345', '2019-09-08 17:12:09'),
-(4, 'ngomahospital', 'ngomaho@gmail.com', '12345', '2019-09-08 18:49:51');
+INSERT INTO `responder` (`id`, `username`, `email`, `password`, `token`, `date_created`) VALUES
+(1, 'ezra', 'pacsonlose@gmail.com', '12345', '', '0000-00-00 00:00:00'),
+(2, 'hospital', 'rubavuhospital@gmail.com', '12345', '', '2019-09-08 16:44:03'),
+(3, 'kacyiruhospital', 'kacyiruho@gmail.com', '12345', '', '2019-09-08 17:12:09'),
+(4, 'ngomahospital', 'ngomaho@gmail.com', '12345', '', '2019-09-08 18:49:51');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `responder`
+--
+ALTER TABLE `responder`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `responder`
+--
+ALTER TABLE `responder`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
--- php/include/dbconfig.php
+++ php/include/dbconfig.php
@@ -2,7 +2,7 @@
 
 define("DB_HOST","localhost");
 define("DB_USER","root");
-define("DB_PASSWORD","");
+define("DB_PASSWORD","root");
 define("DB_DATABASE","emergencyresponder");
 
 $con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
